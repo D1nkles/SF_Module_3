c61d1e81c25633ffbf489535e58a146d535ec812
Ilia <isinetov@mail.ru>
2024-04-19 15:21:30 +0400

    Задание 3.3.2

--- SF_Module_3/Program.cs
+++ SF_Module_3/Program.cs
@@ -5,12 +5,7 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "John";
-
-        Console.WriteLine(myName);
-
-        Console.WriteLine("\u0023");
+        
 
         Console.ReadKey();
     }
---
+++ Task 3_3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿string myName = "Ilia";
+byte age = 22;
+bool pet = true;
+double shoeSize = 42.5;
+
+Console.WriteLine($"My name is {myName}\n" +
+                  $"My age is {age}\n" +
+                  $"Do I have a pet? {pet}\n" +
+                  $"My shoe size is {shoeSize}");
+
+Console.ReadKey();
